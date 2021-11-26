115e0f5192f89d6c22d076a447677a73577e90ff
Kyo1006 <92916283+Kyo1006@users.noreply.github.com>
2021-11-26 19:02:44 +0700

    Update ProductModal.js
    Update rating stars

--- my-app/src/components/ProductModal.js
+++ my-app/src/components/ProductModal.js
@@ -82,8 +82,65 @@ function ProductModal({ lgShow, product, setLgShow, sendMsg }) {
             </Col>
             <Col className='d-none d-md-block' md={6}>
               <p className="text-justify">Thành phần: {product.ingredients}</p>
-              <p className="text-justify">Đánh giá: {product.rating} ({product.numReviews})</p>
-
+              <p className="text-justify">
+                Đánh giá: 
+                <span style={{color:'#f1c40f'}}>
+                  <i 
+                    className={
+                      product.rating >= 1
+                        ? 'fa fa-star'
+                        : product.rating >= 0.5 
+                        ? 'fa fa-star-half'
+                        : ''
+                    }
+                  ></i>
+                </span>
+                <span style={{color:'#f1c40f'}}>
+                  <i 
+                    className={
+                      product.rating >= 2
+                        ? 'fa fa-star'
+                        : product.rating >= 1.5 
+                        ? 'fa fa-star-half'
+                        : ''
+                    }
+                  ></i>
+                </span>
+                <span style={{color:'#f1c40f'}}>
+                  <i 
+                    className={
+                      product.rating >= 3
+                        ? 'fa fa-star'
+                        : product.rating >= 2.5 
+                        ? 'fa fa-star-half'
+                        : ''
+                    }
+                  ></i>
+                </span>
+                <span style={{color:'#f1c40f'}}>
+                  <i 
+                    className={
+                      product.rating >= 4
+                        ? 'fa fa-star'
+                        : product.rating >= 3.5 
+                        ? 'fa fa-star-half'
+                        : ''
+                    }
+                  ></i>
+                </span>
+                <span style={{color:'#f1c40f'}}>
+                  <i 
+                    className={
+                      product.rating >= 5
+                        ? 'fa fa-star'
+                        : product.rating >= 4.5 
+                        ? 'fa fa-star-half'
+                        : ''
+                    }
+                  ></i>
+                </span>
+                ({product.numReviews})
+              </p>
             </Col>
           </Row>
           <br />
